57bc3c504a85f0050cbb494af3e3d8a4da3a6dd7
Vlad <vlad@pop-os.localdomain>
2022-01-29 09:58:36 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Header } from './components/Header';
+
+const App = () => {
+  return(
+    <Header />
+  )
+}
+
+export default App;
